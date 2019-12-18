1890fa10e1dd3900ab28ef1a19e6fa0641369d26
Salih <salih@users.noreply.github.com>
2019-12-18 15:24:14 +0300

    Done

---
+++ Scripts/benim.js
@@ -0,0 +1,24 @@
+﻿var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+
+  if ($(window).scrollTop() >= 200) {       
+    $('#pageup').show();    
+} else {
+    $('#pageup').hide();  
+}
+
+} 
+
+$('#pageup').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                     
+    }, 500);
+});
